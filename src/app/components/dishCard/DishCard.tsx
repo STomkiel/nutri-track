8325e990284bc7a269d0170279d4edf43321f113
Sebastian Tomkiel <sebastiantomkiel@gmail.com>
2024-05-03 21:55:04 +0100

    Created dish card

---
+++ src/app/components/dishCard/DishCard.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import Card from '@mui/material/Card';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import { CardActionArea, Box } from '@mui/material';
+import Image from 'next/image';
+import Link from 'next/link';
+
+interface DishCardProps {
+  nameId: string;
+  typeId: string;
+  description: string;
+  imgSrc: string;
+}
+const DishCard = ({ nameId, typeId, description, imgSrc }: DishCardProps) => {
+  return (
+    <Card sx={{ maxWidth: 345, marginRight: 2 }}>
+      <Link href={`/recipe/${typeId}/${nameId}`}>
+        <CardActionArea>
+          <CardMedia>
+            <Box width="400px" height="300px" position="relative">
+              <Image
+                fill
+                src={imgSrc}
+                alt={nameId}
+                sizes="(max-width: 400px) 100vw, (max-width: 1200px) 50vw, 33vw"
+              />
+            </Box>
+          </CardMedia>
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {nameId.replaceAll('-', ' ')}
+            </Typography>
+            <Typography
+              variant="body2"
+              color="text.secondary"
+              maxHeight="40px"
+              overflow="hidden"
+            >
+              {description}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Link>
+    </Card>
+  );
+};
+
+export default DishCard;
