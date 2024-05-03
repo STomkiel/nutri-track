67fa301bd323c0553691f8f202e23900a5a79d9d
Sebastian Tomkiel <sebastiantomkiel@gmail.com>
2024-05-03 20:53:04 +0100

    Added dish type list

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,5 +1,8 @@
 import PageTitle from '../components/pageTitle/PageTitle';
 import Translation from '../translation/Translation';
+import data from '../../../mockData/foodTypes.json';
+import Link from 'next/link';
+import DishTypeCard from '../components/dishTypeCard/DishTypeCard';
 
 export default function Home() {
   return (
@@ -7,7 +10,18 @@ export default function Home() {
       <div>
         <PageTitle text={<Translation id={'pageTitle'} namespace="Dish" />} />
         <div className="justify-center">
-          <div className="flex flex-wrap justify-center">LIST</div>
+          <div className="flex flex-wrap justify-center">
+            {data.types.map((typeItem) => (
+              <div key={typeItem.name_id} className="m-2">
+                <Link href={`/recipe/${typeItem.name_id}`}>
+                  <DishTypeCard
+                    dishType={typeItem.name_id}
+                    imgSrc={typeItem.img_src}
+                  />
+                </Link>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
